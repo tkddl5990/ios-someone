13113f91a2d96ea689da9e3c73ecb38056354474
tkddl <>
2021-05-25 17:32:33 +0900

    init

---
+++ someoneApplication/someoneApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  someoneApplication
+//
+//  Created by 남상이 on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
